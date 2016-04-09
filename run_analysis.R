954cb1acea819b2ee0d38d1c0cb70fea3daffa3e
smita <smita.mahadik@gmail.com>
2016-04-09 09:57:21 +0800

    updated run_analysis.R to clean variable names

--- run_analysis.R
+++ run_analysis.R
@@ -1,60 +1,66 @@
-setwd("C:/Users/PRATAP/Desktop/1.smita-technical/Data_Scientist/data/UCI HAR Dataset")
-
 library(data.table)
 library(plyr)
 library(dplyr)
 
+# Download the data
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+if (!file.exists("UCI HAR Dataset")) {
+  + download.file(fileURL, destfile="HAR.zip")
+  + unzip("HAR.zip")}
+
 #Read  training data
-features <- read.table("./features.txt")
-xTrain <- read.table("./train/X_train.txt")
-yTrain <- read.table("./train/y_train.txt")
-subjectTrain <- read.table("./train/subject_train.txt")
+features <- read.table("UCI HAR Dataset/features.txt")
+xTrain <- read.table("UCI HAR Dataset/train/X_train.txt")
+yTrain <- read.table("UCI HAR Dataset/train/y_train.txt")
+subjectTrain <- read.table("UCI HAR Dataset/train/subject_train.txt")
 
 #read Test data
-xTest <- read.table("./test/X_test.txt")
-yTest <- read.table("./test/y_test.txt")
-subjectTest <- read.table("./test/subject_test.txt")
+xTest <- read.table("UCI HAR Dataset/test/X_test.txt")
+yTest <- read.table("UCI HAR Dataset/test/y_test.txt")
+subjectTest <- read.table("UCI HAR Dataset/test/subject_test.txt")
 
 #Assign column names to X test  and train data
 colnames(xTrain) <- features[,2]
 colnames(xTest) <- features[,2]
 
-#Add Activities and participants data to X data
-xTrain$activities <- yTrain[,1]
-xTrain$participants <- subjectTrain[,1]
-xTest$activities <- yTest[,1]
-xTest$participants <- subjectTest[,1]
+#Add activity and subject data to X data
+xTrain$activity <- yTrain[,1]
+xTrain$subject <- subjectTrain[,1]
+xTest$activity <- yTest[,1]
+xTest$subject <- subjectTest[,1]
 
 #Merge Train and Test data 
 xMaster <- rbind(xTrain,xTest)
 xMaster <- xMaster[, !duplicated(colnames(xMaster))]
 
 # Extracts only the measurements on the mean and standard deviation for each measurement. 
-meanFeatures <- grep("mean()",ignore.case = TRUE,colnames(xMaster))
-meanData <- xMaster[meanFeatures]
-meanFeatures <- grep("mean()",ignore.case = TRUE,names(xMaster))
-meanData <- xMaster[meanFeatures]
-std <- grep("std()",colnames(xMaster))
-stdData <- xMaster[std]
+filtered_feature_ids <- grepl("mean|std|subject|activity", colnames(xMaster)) & !grepl("meanFreq", colnames(xMaster))
+filteredData <- xMaster[filtered_feature_ids]
 
-#Uses descriptive activity names to name the activities in the data set
-xMaster$activities[xMaster$activities==1] <- "Walking"
-xMaster$activities[xMaster$activities==2] <- "WALKING_UPSTAIRS"
-xMaster$activities[xMaster$activities==3] <- "WALKING_DOWNSTAIRS"
-xMaster$activities[xMaster$activities==4] <- "SITTING"
-xMaster$activities[xMaster$activities==5] <- "STANDING"
-xMaster$activities[xMaster$activities==6] <- "LAYING"
+#Uses descriptive activity names to name the activity in the data set
+filteredData$activity[filteredData$activity==1] <- "Walking"
+filteredData$activity[filteredData$activity==2] <- "WALKING_UPSTAIRS"
+filteredData$activity[filteredData$activity==3] <- "WALKING_DOWNSTAIRS"
+filteredData$activity[filteredData$activity==4] <- "SITTING"
+filteredData$activity[filteredData$activity==5] <- "STANDING"
+filteredData$activity[filteredData$activity==6] <- "LAYING"
 
 #Appropriately labels the data set with descriptive variable names
-colnames(xMaster) <- gsub("Acc","Accelerator",colnames(xMaster))
-colnames(xMaster) <- gsub("Gyro","Gyroscope",colnames(xMaster))
-colnames(xMaster) <- gsub("Mag","Magnitude",colnames(xMaster))
-colnames(xMaster) <- gsub("^f","Frequency",colnames(xMaster))
-colnames(xMaster) <- gsub("^t","Time",colnames(xMaster))
+featureNames <- colnames(filteredData)
+featureNames <- gsub("Acc","-acceleration",featureNames)
+featureNames <- gsub("(Gyro|Jerk)","-\\1",featureNames)
+featureNames <- gsub("Mag","-magnitude",featureNames)
+featureNames <- gsub("^f(.*)$","\\1-frequency",featureNames)
+featureNames <- gsub("^t(.*)$","\\1-time",featureNames)
+featureNames <- gsub("\\(\\)","",featureNames)
+featureNames <- gsub("BodyBody","body",featureNames)
+featureNames <- tolower(as.list(featureNames))
 
-xMaster$participants <- paste("Participant", xMaster$participants)
+colnames(filteredData) <- featureNames
+
+filteredData$subject <- paste("Participant", filteredData$subject)
 
 #create Tidy data
-xMaster <- data.table(xMaster)
-tidyData <- xMaster[,lapply(.SD,mean),by= 'participants,activities']
-write.table(tidyData, file = "Tidy.txt", row.names = FALSE)
+filteredData <- data.table(filteredData)
+tidyData <- filteredData[,lapply(.SD,mean),by= 'subject,activity']
+write.table(tidyData, file = "tidyData.txt", row.names = FALSE)
